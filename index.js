e1e55e19b6a7c389a8739603acc00b888c515581
Tarun Rohith Kodi <93774503+TARUNROHITH@users.noreply.github.com>
2024-06-16 22:43:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,240 @@
+import A0 from "./img-seq0000.jpg";
+import A1 from "./img-seq0001.jpg";
+import A2 from "./img-seq0002.jpg";
+import A3 from "./img-seq0003.jpg";
+import A4 from "./img-seq0004.jpg";
+import A5 from "./img-seq0005.jpg";
+import A6 from "./img-seq0006.jpg";
+import A7 from "./img-seq0007.jpg";
+import A8 from "./img-seq0008.jpg";
+import A9 from "./img-seq0009.jpg";
+import A10 from "./img-seq0010.jpg";
+import A11 from "./img-seq0011.jpg";
+import A12 from "./img-seq0012.jpg";
+import A13 from "./img-seq0013.jpg";
+import A14 from "./img-seq0014.jpg";
+import A15 from "./img-seq0015.jpg";
+import A16 from "./img-seq0016.jpg";
+import A17 from "./img-seq0017.jpg";
+import A18 from "./img-seq0018.jpg";
+import A19 from "./img-seq0019.jpg";
+import A20 from "./img-seq0020.jpg";
+import A21 from "./img-seq0021.jpg";
+import A22 from "./img-seq0022.jpg";
+import A23 from "./img-seq0023.jpg";
+import A24 from "./img-seq0024.jpg";
+import A25 from "./img-seq0025.jpg";
+import A26 from "./img-seq0026.jpg";
+import A27 from "./img-seq0027.jpg";
+import A28 from "./img-seq0028.jpg";
+import A29 from "./img-seq0029.jpg";
+import A30 from "./img-seq0030.jpg";
+import A31 from "./img-seq0031.jpg";
+import A32 from "./img-seq0032.jpg";
+import A33 from "./img-seq0033.jpg";
+import A34 from "./img-seq0034.jpg";
+import A35 from "./img-seq0035.jpg";
+import A36 from "./img-seq0036.jpg";
+import A37 from "./img-seq0037.jpg";
+import A38 from "./img-seq0038.jpg";
+import A39 from "./img-seq0039.jpg";
+import A40 from "./img-seq0040.jpg";
+import A41 from "./img-seq0041.jpg";
+import A42 from "./img-seq0042.jpg";
+import A43 from "./img-seq0043.jpg";
+import A44 from "./img-seq0044.jpg";
+import A45 from "./img-seq0045.jpg";
+import A46 from "./img-seq0046.jpg";
+import A47 from "./img-seq0047.jpg";
+import A48 from "./img-seq0048.jpg";
+import A49 from "./img-seq0049.jpg";
+import A50 from "./img-seq0050.jpg";
+import A51 from "./img-seq0051.jpg";
+import A52 from "./img-seq0052.jpg";
+import A53 from "./img-seq0053.jpg";
+import A54 from "./img-seq0054.jpg";
+import A55 from "./img-seq0055.jpg";
+import A56 from "./img-seq0056.jpg";
+import A57 from "./img-seq0057.jpg";
+import A58 from "./img-seq0058.jpg";
+import A59 from "./img-seq0059.jpg";
+import A60 from "./img-seq0060.jpg";
+import A61 from "./img-seq0061.jpg";
+import A62 from "./img-seq0062.jpg";
+import A63 from "./img-seq0063.jpg";
+import A64 from "./img-seq0064.jpg";
+import A65 from "./img-seq0065.jpg";
+import A66 from "./img-seq0066.jpg";
+import A67 from "./img-seq0067.jpg";
+import A68 from "./img-seq0068.jpg";
+import A69 from "./img-seq0069.jpg";
+import A70 from "./img-seq0070.jpg";
+import A71 from "./img-seq0071.jpg";
+import A72 from "./img-seq0072.jpg";
+import A73 from "./img-seq0073.jpg";
+import A74 from "./img-seq0074.jpg";
+import A75 from "./img-seq0075.jpg";
+import A76 from "./img-seq0076.jpg";
+import A77 from "./img-seq0077.jpg";
+import A78 from "./img-seq0078.jpg";
+import A79 from "./img-seq0079.jpg";
+import A80 from "./img-seq0080.jpg";
+import A81 from "./img-seq0081.jpg";
+import A82 from "./img-seq0082.jpg";
+import A83 from "./img-seq0083.jpg";
+import A84 from "./img-seq0084.jpg";
+import A85 from "./img-seq0085.jpg";
+import A86 from "./img-seq0086.jpg";
+import A87 from "./img-seq0087.jpg";
+import A88 from "./img-seq0088.jpg";
+import A89 from "./img-seq0089.jpg";
+import A90 from "./img-seq0090.jpg";
+import A91 from "./img-seq0091.jpg";
+import A92 from "./img-seq0092.jpg";
+import A93 from "./img-seq0093.jpg";
+import A94 from "./img-seq0094.jpg";
+import A95 from "./img-seq0095.jpg";
+import A96 from "./img-seq0096.jpg";
+import A97 from "./img-seq0097.jpg";
+import A98 from "./img-seq0098.jpg";
+import A99 from "./img-seq0099.jpg";
+import A100 from "./img-seq0100.jpg";
+import A101 from "./img-seq0101.jpg";
+import A102 from "./img-seq0102.jpg";
+import A103 from "./img-seq0103.jpg";
+import A104 from "./img-seq0104.jpg";
+import A105 from "./img-seq0105.jpg";
+import A106 from "./img-seq0106.jpg";
+import A107 from "./img-seq0107.jpg";
+import A108 from "./img-seq0108.jpg";
+import A109 from "./img-seq0109.jpg";
+import A110 from "./img-seq0110.jpg";
+import A111 from "./img-seq0111.jpg";
+
+
+export const AirPods = Array.of(
+    A0,
+    A1,
+    A2,
+    A3,
+    A4,
+    A5,
+    A6,
+    A7,
+    A8,
+    A9,
+  
+    A10,
+    A11,
+    A12,
+    A13,
+    A14,
+    A15,
+    A16,
+    A17,
+    A18,
+    A19,
+  
+    A20,
+    A21,
+    A22,
+    A23,
+    A24,
+    A25,
+    A26,
+    A27,
+    A28,
+    A29,
+  
+    A30,
+    A31,
+    A32,
+    A33,
+    A34,
+    A35,
+    A36,
+    A37,
+    A38,
+    A39,
+  
+    A40,
+    A41,
+    A42,
+    A43,
+    A44,
+    A45,
+    A46,
+    A47,
+    A48,
+    A49,
+  
+    A50,
+    A51,
+    A52,
+    A53,
+    A54,
+    A55,
+    A56,
+    A57,
+    A58,
+    A59,
+  
+    A60,
+    A61,
+    A62,
+    A63,
+    A64,
+    A65,
+    A66,
+    A67,
+    A68,
+    A69,
+  
+    A70,
+    A71,
+    A72,
+    A73,
+    A74,
+    A75,
+    A76,
+    A77,
+    A78,
+    A79,
+  
+    A80,
+    A81,
+    A82,
+    A83,
+    A84,
+    A85,
+    A86,
+    A87,
+    A88,
+    A89,
+  
+    A90,
+    A91,
+    A92,
+    A93,
+    A94,
+    A95,
+    A96,
+    A97,
+    A98,
+    A99,
+  
+    A100,
+    A101,
+    A102,
+    A103,
+    A104,
+    A105,
+    A106,
+    A107,
+    A108,
+    A109,
+  
+    A110,
+    A111
+  );
+  
